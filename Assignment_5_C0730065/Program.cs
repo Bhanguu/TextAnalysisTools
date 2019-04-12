ffad65d2e33085b3859dd9ff9d00e085c827110d
visualstudio <visualstudio@122-PC22>
2019-04-12 18:59:24 -0400

    TextAnalaysisTools

--- Assignment_5_C0730065/Program.cs
+++ Assignment_5_C0730065/Program.cs
@@ -8,90 +8,66 @@ namespace Assignment_5_C0730065
 {
     //Parminder kaur bhangu
     //c0730065
-    class Program 
+    class Program
     {
-      
-            string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        public static void Main()
 
 
 
-            public static void Main()
+        {
+
+            Program a = new Program();
+
+            Console.WriteLine("the lomgest word is: ");
+
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for procesing bt an analytical Engine."));
+
+
+
+        }
+
+        private bool MyFunctionA(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyFunctionA()
+
+        {
+
+            string[] words = input.Split('');
+
+            int wordArrayLength = new int[wordArrayLength];
+
+
+
+            foreach (var word in words)
 
             {
 
 
-
-                Program orange = new Program();
-
-
-
-                Console.WriteLine(orange.Gematria("universe"));
-             
-
-
-                Console.ReadLine();
 
             }
 
+            string currentWord = words[0];
 
+            string nextWord;
 
-            public int Gematria(string word)
-
-            {
-
-
-
-                int GematriaValue = 0;
-
-
-
-                for (int i = 0; i < word.Length; i++)
-
-
-
-                {
-
-                    GematriaValue += LetterValue(word[i].ToString());
-
-                }
-
-
-
-                return GematriaValue;
-
-            }
-
-
-
-            public int LetterValue(string letter)
+            for (int y = 0; y < words.Length - 1; y++)
 
             {
 
+                currentWord = words[y];
+
+                nextWord = words[y + 1];
 
 
-                int x = 0;
-
-
-
-                while (Letters[x] != letter)
-
-
-
-                {
-
-
-
-                    if (Letters[x++] == letter)
-
-                        return -1;
-
-                }
-
-                return x + 1;
 
             }
 
         }
-    }
-    
 
+    }
+}
+        
