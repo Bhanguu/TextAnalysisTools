cca4558e95f2165783b12e1ece06c73c19ddbf5f
visualstudio <visualstudio@122-PC22>
2019-04-12 18:30:20 -0400

    Gematria

--- Assignment_5_C0730065/Program.cs
+++ Assignment_5_C0730065/Program.cs
@@ -10,8 +10,88 @@ namespace Assignment_5_C0730065
     //c0730065
     class Program 
     {
-        static void Main(string[] args)
-        {
+      
+            string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+
+
+            public static void Main()
+
+            {
+
+
+
+                Program orange = new Program();
+
+
+
+                Console.WriteLine(orange.Gematria("universe"));
+
+
+
+                Console.ReadLine();
+
+            }
+
+
+
+            public int Gematria(string word)
+
+            {
+
+
+
+                int GematriaValue = 0;
+
+
+
+                for (int i = 0; i < word.Length; i++)
+
+
+
+                {
+
+                    GematriaValue += LetterValue(word[i].ToString());
+
+                }
+
+
+
+                return GematriaValue;
+
+            }
+
+
+
+            public int LetterValue(string letter)
+
+            {
+
+
+
+                int x = 0;
+
+
+
+                while (Letters[x] != letter)
+
+
+
+                {
+
+
+
+                    if (Letters[x++] == letter)
+
+                        return -1;
+
+                }
+
+                return x + 1;
+
+            }
+
         }
     }
-}
+    
+
